575f56b87bec4a8d9116ddf267a93412d4d1252a
Mónica Restrepo L <65642162+monicasofiarestrepo@users.noreply.github.com>
2023-04-17 16:38:23 -0500

    Add files via upload

---
+++ futbolista.py
@@ -0,0 +1,42 @@
+from persona import Persona
+from deportista import Deportista
+
+class Futbolista(Persona,Deportista):
+    listaFutbolistas=[]
+    def __init__(self,nombre,edad,altura,sexo,añosPracticando,golesMarcados,tarjetasRojas,piernaHabil):
+        Persona.__init__(self,nombre,edad,altura,sexo)
+        Deportista.__init__(self,"Futbol",añosPracticando)
+        self._golesMarcados=golesMarcados
+        self._tarjetasRojas=tarjetasRojas
+        self._piernaHabil=piernaHabil
+        Futbolista.listaFutbolistas.append(self)
+
+    def __str__(self):
+        return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
+    
+    def getGolesMarcados(self):
+        return self._golesMarcados
+
+    def setGolesMarcados(self, golesMarcados):
+        self._golesMarcados = golesMarcados
+
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+
+    def setTarjetasRojas(self, tarjetasRojas):
+        self._tarjetasRojas = tarjetasRojas
+
+    def getPiernaHabil(self):
+        return self._piernaHabil
+
+    def setPiernaHabil(self, piernaHabil):
+        self._piernaHabil = piernaHabil
+
+
+
+    @classmethod
+    def get_listaFutbolista(cls):
+        return Futbolista.listaFutbolistas
+    @classmethod
+    def set_listaFutbolista(cls, listaFutbolista):
+        Futbolista.listaFutbolistas = listaFutbolista
